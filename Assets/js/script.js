fa8b72c50e3de0aa806fb49ae344e5ee9217f09f
Jelenarog <jelena.rogic90@gmail.com>
2022-11-14 22:14:12 -0700

    added Readme

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,23 +1,24 @@
-
 $(function () {
- 
   var saveButton = $(".saveBtn");
   var currentDate = $("#currentDay");
   var currentBlock = $(".time-block");
   //code to display the current date in the header of the page.
   var today = dayjs();
-  currentDate.text(today.format("dddd ,MMMM DD YYYY"));// display current date as weekday, month,day in month and year
+  currentDate.text(today.format("dddd ,MMMM DD YYYY")); // display current date as weekday, month,day in month and year
 
-  saveButton.on("click", function () { //event listener for each button, function looks for buttons parents ID and for each it stores text area value
-    localStorage.setItem($(this).parent().attr("id"), $(this).prev().val());//
+  saveButton.on("click", function () {
+    //event listener for each button, function looks for buttons parents ID and for each it stores text area value
+    localStorage.setItem($(this).parent().attr("id"), $(this).prev().val()); //
   });
 
-
- for (let i = 0; i < localStorage.length; i++) {//for each local storage 
-     currentTask = localStorage.key(i);
-     //for each id with value of local storage key for html textarea value input local storage value of the same key
-     $('#' + currentTask).children(1).val(localStorage.getItem(currentTask));
- }
+  for (let i = 0; i < localStorage.length; i++) {
+    //for each local storage
+    currentTask = localStorage.key(i);
+    //for each id with value of local storage key for html textarea value input local storage value of the same key
+    $("#" + currentTask)
+      .children(1)
+      .val(localStorage.getItem(currentTask));
+  }
 
   // this is current time
   var currentTime = dayjs().format("H");
@@ -35,4 +36,3 @@ $(function () {
     }
   });
 });
-
--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# Work-Day-Scheduler
+# Work Day Scheduler
+
+## Description
+This project helps us with our organization during our busy schedule. Page displays our working hours, and each hour has a block on its own where you can input your to do task.
+if you click on save button for each task data will save to your local storage and you will be able to access it even if the page is refreshed.
+
+---
+## Installation 
+* Clone the repository  
+```
+git clone git@github.com:Jelenarog/Work-Day-Scheduler.git
+```
+---
+## Usage
+![User-story](./Assets/Images/User-story.png)
+
+---
+## Links
+[Github Repository Link](https://github.com/Jelenarog/Quiz)<br/>
+ [Deployed website](https://jelenarog.github.io/Work-Day-Scheduler/)<br/>
+ [HTML code](.//Assets/Images/HTML%20code.png)<br/>
+ [JavaScript](.//Assets/Images/Java.sc.png)
+
+---
+
+## Credits
+ [Starter code](https://github.com/coding-boot-camp/crispy-octo-meme)<br/>
+
+---
+
+ ---
+
+## License 
+
+Please refer to the LICENSE in the repo.
+
+---
