1a83231c2d0cf931134bd6394053ccf68ee4eeaf
Jelenarog <jelena.rogic90@gmail.com>
2022-11-14 18:30:13 -0700

    getting local storage data and saving it as value

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,100 +1,38 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 
-newFunction();
-
-
-function newFunction() {
-  var hourList = $("#hour");
-  var saveButton = $('.saveBtn');
-  var wrap = $(".container-fluid");
-  var currentDate = $('#currentDay');
-  var currentBlock = $(".time-block");
-  var userInput = $("<textarea>");
-  var taskInput = $("#input");
-  $(function () {
-
-    //code to display the current date in the header of the page.
-    var today = dayjs();
-    currentDate.text(today.format('dddd ,MMMM DD YYYY'));
-    saveButton.on('click', function (event) {
-      //event.preventDefault()
-      var target = event.target;
-      if (target.matches('button')) {
-        let text = [];
-        for (let i = 0; i < taskInput.length; i++) {
-          text.push(taskInput.val());
-          console.log(text);
-          localStorage.setItem("userInput", JSON.stringify(text));
-          var task = JSON.parse(localStorage.getItem("userInput"));
-          //console.log(task);
-          taskInput.text(task);
-          console.log("Button works");
-
-
-        }
-        //     text.push(taskInput.val());
-        //     console.log(text);
-        // localStorage.setItem("userInput",JSON.stringify(text))
-        // var task = JSON.parse(localStorage.getItem("userInput"));
-        // console.log(task);
-        // taskInput.text(task);
-        // console.log("Button works");  
-      }
-    });
-
-
-    //save past tasks
-    function pastTasks() {
-
-      var storedTask = JSON.parse(localStorage.getItem("userInput"));
-      if (storedTask !== null) {
-        task = storedTask;
-      }
-    }
-    pastTasks();
-
-    currentBlock.each(function () {
-      var blockID = $(this).attr('id');
-      //console.log(blockID);
-      //this is current time 
-      var currentTime = dayjs().format('H');
-      //console.log(currentTime);
-      if (currentTime = blockID) {
-        currentBlock.addClass('present');
-      }
-      else if (currentTime < blockID) {
-        currentBlock.addClass('past');
-      }
-      else {
-        currentBlock.addClass('future');
-      }
-
-    });
-    pastTasks();
-  });
-}
-//  var blockID = wrap.children().children(1).eq(0).val();
-//  console.log(blockID);
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
+$(function () {
  
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
+  var saveButton = $(".saveBtn");
+  var currentDate = $("#currentDay");
+  var currentBlock = $(".time-block");
+  //code to display the current date in the header of the page.
+  var today = dayjs();
+  currentDate.text(today.format("dddd ,MMMM DD YYYY"));// display current date as weekday, month,day in month and year
 
-// TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
+  saveButton.on("click", function () { //event listener for each button, function looks for buttons parents ID and for each it stores text area value
+    localStorage.setItem($(this).parent().attr("id"), $(this).prev().val());//
+  });
+
+
+ for (let i = 0; i < localStorage.length; i++) {//for each local storage 
+     currentTask = localStorage.key(i);
+     //for each id with value of local storage key for html textarea value input local storage value of the same key
+     $('#' + currentTask).children(1).val(localStorage.getItem(currentTask));
+ }
+
+  // this is current time
+  var currentTime = dayjs().format("H");
+  currentTime = Number(currentTime);
+  //compare current time with id of each block and apply appropriate style class based on condition
+  $(currentBlock).each(function () {
+    var blockID = $(this).attr("id");
+    blockID = Number(blockID);
+    if (currentTime == blockID) {
+      $(this).addClass("present");
+    } else if (currentTime > blockID) {
+      $(this).addClass("past");
+    } else {
+      $(this).addClass("future");
+    }
+  });
+});
 
